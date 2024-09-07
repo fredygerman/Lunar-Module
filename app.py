32f7ba31765935faefca46e86b7b764751a0c01b
eddiegulay <edgargulay@outlook.com>
2024-09-07 16:07:44 +0300

    resolve: request method resolution

--- app.py
+++ app.py
@@ -3,6 +3,8 @@ import json
 from typing import List
 import os
 
+from utils.utilities import build_menu
+
 ROOT_DIR = os.getcwd()
 
 app = FastAPI()
@@ -11,14 +13,20 @@ app = FastAPI()
 with open(os.path.join(ROOT_DIR, "data/game_reserves.json"), "r") as f:
     game_reserves = json.load(f)
 
-@app.get("/game-reserves/{region_name}", response_model=List[dict])
+@app.post("/game-reserves/{region_name}", response_model=dict)
 async def get_game_reserves_by_region(region_name: str):
     reserves = [reserve for reserve in game_reserves if reserve["region"].lower() == region_name.lower()]
+    reserves = build_menu(
+        body="Please select what game reserve that you are going to visit.",
+        button="Game Reserve",
+        title="Game reserves",
+        rows=reserves
+    )
     if not reserves:
         raise HTTPException(status_code=404, detail="No game reserves found for the given region")
     return reserves
 
-@app.get("/region/{reserve_name}")
+@app.post("/region/{reserve_name}", response_model=dict)
 async def get_region_by_reserve(reserve_name: str):
     for reserve in game_reserves:
         _reserve_name = reserve_name.lower()
