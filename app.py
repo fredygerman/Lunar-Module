f142b00697077d2f2312539c901f3fb6b759a79c
Fredy German <fredgerman30@gmail.com>
2024-09-07 16:29:09 +0300

    fix : Request import

--- app.py
+++ app.py
@@ -1,5 +1,4 @@
-from urllib.request import Request
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, HTTPException, Request
 import json
 from typing import List
 import os
@@ -22,9 +21,8 @@ async def get_game_reserves_by_region(region_name: str):
 @app.get("/region/{reserve_name}")
 async def get_region_by_reserve(reserve_name: str):
     for reserve in game_reserves:
-        _reserve_name = reserve_name.lower()
-        if reserve["national_park"].lower() == _reserve_name or reserve['national_park'] in reserve_name.lower():
-            return {"id": reserve["id"],"region": reserve["region"]}
+        if reserve["name"].lower() == reserve_name.lower():
+            return {"region": reserve["region"]}
     raise HTTPException(status_code=404, detail="Game reserve not found")
 
 @app.post("/validate-destination")
