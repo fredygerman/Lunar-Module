903872f33113f5499a1c17ee46e3dcdd59ff60e6
eddiegulay <edgargulay@outlook.com>
2024-09-07 16:33:02 +0300

    reserve lookup

--- app.py
+++ app.py
@@ -13,9 +13,10 @@ app = FastAPI()
 with open(os.path.join(ROOT_DIR, "data/game_reserves.json"), "r") as f:
     game_reserves = json.load(f)
 
-@app.post("/game-reserves/{region_name}", response_model=dict)
-async def get_game_reserves_by_region(region_name: str):
-    reserves = [reserve for reserve in game_reserves if reserve["region"].lower() == region_name.lower()]
+@app.post("/game-reserves", response_model=dict)
+async def get_game_reserves_by_region(data: dict):
+    region_name = data.get('region_name')
+    reserves = [reserve for reserve in game_reserves if reserve["region"].lower() == region_name.lower()][:5]
     reserves = build_menu(
         body="Please select what game reserve that you are going to visit.",
         button="Game Reserve",
@@ -23,15 +24,17 @@ async def get_game_reserves_by_region(region_name: str):
         rows=reserves
     )
     if not reserves:
-        raise HTTPException(status_code=404, detail="No game reserves found for the given region")
-    return reserves
+        return {"text": "Sorry, your option is not available at the moment please write region name like Arusha, Dodoma etc"}
 
-@app.post("/region/{reserve_name}", response_model=dict)
-async def get_region_by_reserve(reserve_name: str):
+
+@app.post("/region", response_model=dict)
+async def get_region_by_reserve(data: dict):
+    reserve_name = data.get('reserve_name')
     for reserve in game_reserves:
         _reserve_name = reserve_name.lower()
         if reserve["national_park"].lower() == _reserve_name or reserve['national_park'] in reserve_name.lower():
             return {"id": reserve["id"],"region": reserve["region"]}
-    raise HTTPException(status_code=404, detail="Game reserve not found")
+    raise {"text":"Game reserve not found, you can try "}
+
 
 
